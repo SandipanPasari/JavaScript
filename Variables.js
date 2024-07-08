e5ac05cbaef5837bfcb7a9f919cf19fe82066c05
SandipanPasari <139198423+SandipanPasari@users.noreply.github.com>
2024-07-08 15:33:56 +0530

    Updated

--- Variables.js
+++ Variables.js
@@ -2,6 +2,7 @@ const accountID = 14453;
 let accountEmail = "Sandi@gmail.com";
 var accountPassword = "123";
 accountCity = "Kolkata";
+let accountState;
 
 // accountID = 2; // constant can't be changed
 accountEmail = "Sp@gmail.com";
@@ -15,4 +16,4 @@ Prefer not to use var
 because of issue in block scope and functional scope
 */
 
-console.table([accountID, accountEmail, accountCity]);
+console.table([accountID, accountEmail, accountCity, accountState]);
