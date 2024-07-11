af6918f0e5753b7b9b25e12435dec072f2bf0b80
SandipanPasari <139198423+SandipanPasari@users.noreply.github.com>
2024-07-11 12:07:32 +0530

    maths

---
+++ 01_BASICS/Maths.js
@@ -0,0 +1,17 @@
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.33));
+// console.log(Math.round(4.9));
+
+// console.log(Math.ceil(4.33)); // highest value
+// console.log(Math.floor(4.33)); // lowest value
+
+// console.log(Math.min(4, 9, 5, 1));
+// console.log(Math.max(4, 9, 5, 1));
+
+// console.log(Math.random());
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 1;
+const max = 6;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
---
+++ 01_BASICS/Numbers.js
@@ -0,0 +1,16 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 25.264;
+console.log(otherNumber.toPrecision(3));
+console.log(otherNumber.toPrecision(2));
+
+const hundreds = 100000000000;
+console.log(hundreds.toLocaleString());
