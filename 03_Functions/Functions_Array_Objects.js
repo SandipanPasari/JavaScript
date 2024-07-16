3df7c4d83e22f6e262332f6861817cc47d2e2d7d
SandipanPasari <139198423+SandipanPasari@users.noreply.github.com>
2024-07-16 10:39:16 +0530

    function

---
+++ 03_Functions/Functions_Array_Objects.js
@@ -0,0 +1,27 @@
+function calculateCartPrice(...num1) {
+  return num1;
+}
+console.log(calculateCartPrice(200, 5000, 145));
+
+const user = {
+  username: "Sandipan",
+  price: 99,
+};
+
+function handleObject(anyobject) {
+  console.log(
+    `Username is ${anyobject.username} and price is ${anyobject.price}`
+  );
+}
+handleObject(user);
+
+handleObject({
+  username: "Sam",
+  price: 399,
+});
+
+const myNewArray = [200, 400, 500, 700];
+function returnSecondValue(getArray) {
+  return getArray[1];
+}
+console.log(returnSecondValue(myNewArray));
