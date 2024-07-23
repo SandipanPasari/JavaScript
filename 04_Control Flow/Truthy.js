297c72e2835a5eda0fd93f23f90edd7fe772241b
SandipanPasari <139198423+SandipanPasari@users.noreply.github.com>
2024-07-23 17:07:56 +0530

    add

--- 04_Control Flow/Truthy.js
+++ 04_Control Flow/Truthy.js
@@ -4,10 +4,25 @@ if (userEmail) {
 } else {
   console.log("Don't got the user email");
 }
-
 const userEmail2 = "";
 if (userEmail2) {
   console.log("Got the user Email");
 } else {
   console.log("Don't got the user email");
 }
+
+// Nullish Coalesing operator(??): null undefined
+
+let val1;
+// val1 = 5 ?? 10;
+// val1 = null ?? 10;
+// val1 = undefined ?? 15;
+val1 = null ?? 10 ?? 60;
+
+console.log(val1);
+
+// Terniary Operator
+// condition ? true : false
+
+const iceTeaPrice = 100;
+iceTeaPrice >= 80 ? console.log("Less than 80") : console.log("more than 80");
